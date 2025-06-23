97cd220f4931d75a8b8736073b1b9ef31b9f939f
marc <147423007+marcaixs@users.noreply.github.com>
2025-06-23 16:59:25 +0200

    add create todo cards and print

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -13,6 +13,37 @@ export function printProjects(projects){
     })
 }
 
-export function printTodos(project){
+export function createTodoCard(todo){
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-div');
+
+    const deleteButton = document.createElement('button')
     
+    const title = document.createElement('p');
+    const description = document.createElement('p');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+
+    title.innerText = 'Title: '+todo.title;
+    description.innerText = 'Description: '+todo.description;
+    dueDate.innerText = 'Due Date: '+todo.dueDate;
+    priority.innerText = 'Priority: '+todo.priority;
+
+    todoDiv.appendChild(title);
+    todoDiv.appendChild(description);
+    todoDiv.appendChild(dueDate);
+    todoDiv.appendChild(priority);
+    todoDiv.appendChild(deleteButton);
+
+    return todoDiv;
+}
+
+export function printTodos(project){
+    const todoContainer = document.querySelector('.todobox');
+
+    const todos = project.getTodos();
+    todos.map((todo)=>{
+        const todoDiv = createTodoCard(todo);
+        todoContainer.appendChild(todoDiv);
+    })
 }
