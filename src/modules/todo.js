16bfd41cc5fb39318dfc0cfd0fa6995381da99f3
marc <147423007+marcaixs@users.noreply.github.com>
2025-06-23 18:11:11 +0200

    add id to constructor

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,5 +1,6 @@
 export class Todo {
-    constructor(title, description, dueDate, priority){
+    constructor(title, description, dueDate, priority, id=Math.floor(Math.random() * 100)){
+        this.id = id;
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
