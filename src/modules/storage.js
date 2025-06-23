5198509c8f287a51a9f0987c243550ad21ed69b1
marc <147423007+marcaixs@users.noreply.github.com>
2025-06-23 16:09:31 +0200

    first storage functions

---
+++ src/modules/storage.js
@@ -0,0 +1,18 @@
+
+
+export function saveProjectToLocalStorage(projects){
+
+    localStorage.setItem(projects, JSON.stringify(projects))
+}
+
+export function getProjectFromLocalStorage(){
+    const storedProjects = localStorage.getItem('projects')
+
+    if (storedUserData) {
+    const projects = JSON.parse(storedProjects)
+    return projects
+    
+    } else {
+    console.log('Projects not found in local storage')
+    }
+}
