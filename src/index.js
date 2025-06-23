e2741cc2457862b23cd544f1d32c564da8e4f30d
marc <147423007+marcaixs@users.noreply.github.com>
2025-06-23 16:08:25 +0200

    print and save projects

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import "./styles.css";
-import {Project} from './modules/project.js'
+import {Project} from './modules/Project.js'
+import { saveProjectToLocalStorage, getProjectFromLocalStorage } from "./modules/storage.js";
+import { printProjects } from "./modules/ui.js";
+
+const projects = [];
+let currentProject; 
 
 function startTodoList(){
-    const exampleProject = new Project(example);
+    const exampleProject = new Project('example');
+    projects.push(exampleProject)
+    saveProjectToLocalStorage(projects);
+    printProjects(projects);
 }
+
+startTodoList()
 
 const newProjectButton = document.querySelector('.new-project');
 
 newProjectButton.addEventListener('click', ()=>{
     const name = prompt('Enter the new project name', 'New project name')
     const project = new Project(name);
-    
+    projects.push(project);
+    saveProjectToLocalStorage(projects);
+    printProjects(projects); 
 })
+
+
+
